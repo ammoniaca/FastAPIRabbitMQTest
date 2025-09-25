7cef9a963165f42bf7d3011fc62bcc69d35db1a2
Alessandro Montaghi <alessandro.montaghi@gmail.com>
2025-09-25 19:14:08 +0200

    some refactoring to clean stuff welcome

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from src.ingestion.router import router as controller_router
 from src.rabbitmq.router import router as rabbitmq_router
 from src.config import settings
@@ -30,9 +31,9 @@ app = FastAPI(
     openapi_url="/api/v1/openapi.json"
 )
 
-@app.get("/")
-async def root():
-    return {"message": "demo!"}
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+async def welcome():
+    return "<h1>Benvenuto nella mia API!</h1><p>Vai su <a href='/api/v1/docs'>Docs</a></p>"
 
 app.include_router(controller_router)
 app.include_router(rabbitmq_router)
